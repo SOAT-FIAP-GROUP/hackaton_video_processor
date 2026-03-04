dc5a87201fc57ca2d22f2d6ae91b6b9dcf7529cb
Rogerio Inacio <rogerio.reaper@gmail.com>
2026-03-04 19:29:18 -0300

    feat: add video entity

---
+++ apps/shared/database/repository/entities.go
@@ -0,0 +1,11 @@
+package repository
+
+import "time"
+
+type VideoEntity struct {
+	ID          int64     `db:"id"`
+	UserID      string    `db:"user_id"`
+	Name        string    `db:"name"`
+	Path        string    `db:"path"`
+	ProcessedAt time.Time `db:"processed_at"`
+}
