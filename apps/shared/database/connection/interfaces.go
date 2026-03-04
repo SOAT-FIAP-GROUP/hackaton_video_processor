a3707566eacbc50779469e7a3415522764b2ba6f
Rogerio Inacio <rogerio.reaper@gmail.com>
2026-03-04 19:29:34 -0300

    feat: add method to interface

--- apps/shared/database/connection/interfaces.go
+++ apps/shared/database/connection/interfaces.go
@@ -1,6 +1,12 @@
 package connection
 
+import (
+	"context"
+	"database/sql"
+)
+
 type DatabaseConnection interface {
 	Test() error
 	Close() error
+	QueryRow(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
 }
