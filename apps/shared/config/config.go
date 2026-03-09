1f3889f1263c6a0063d6669d7c1c0e5eb31b1807
Rogerio Inacio <rogerio.reaper@gmail.com>
2026-03-09 17:48:52 -0300

    feat: add notification var

--- apps/shared/config/config.go
+++ apps/shared/config/config.go
@@ -7,22 +7,23 @@ import (
 )
 
 type Config struct {
-	CognitoClientID     string `env:"COGNITO_CLIENT_ID,required"`
-	CognitoClientSecret string `env:"COGNITO_CLIENT_SECRET,required"`
-	CognitoUserPoolID   string `env:"COGNITO_USER_POOL_ID,required"`
-	AWSRegion           string `env:"AWS_REGION,required"`
-	AWSAccessKeyID      string `env:"AWS_ACCESS_KEY_ID,required"`
-	AWSSecretAccessKey  string `env:"AWS_SECRET_ACCESS_KEY,required"`
-	AWSS3Bucket         string `env:"AWS_S3_BUCKET,required"`
-	AWSSQSQueueName     string `env:"AWS_SQS_QUEUE_NAME,required"`
-	TempPath            string `env:"TEMP_PATH,required"`
-	DBHost              string `env:"DB_HOST,required"`
-	DBPort              int    `env:"DB_PORT,required"`
-	DBUser              string `env:"DB_USER,required"`
-	DBPassword          string `env:"DB_PASSWORD,required"`
-	DBName              string `env:"DB_NAME,required"`
-	DBSSLMode           string `env:"DB_SSL_MODE,required"`
-	NumberWorkers       int    `env:"NUMBER_WORKERS,required"`
+	CognitoClientID                string `env:"COGNITO_CLIENT_ID,required"`
+	CognitoClientSecret            string `env:"COGNITO_CLIENT_SECRET,required"`
+	CognitoUserPoolID              string `env:"COGNITO_USER_POOL_ID,required"`
+	AWSRegion                      string `env:"AWS_REGION,required"`
+	AWSAccessKeyID                 string `env:"AWS_ACCESS_KEY_ID,required"`
+	AWSSecretAccessKey             string `env:"AWS_SECRET_ACCESS_KEY,required"`
+	AWSS3Bucket                    string `env:"AWS_S3_BUCKET,required"`
+	AWSSQSQueueNameVideoProcessing string `env:"AWS_SQS_QUEUE_NAME_VIDEO_PROCESSING,required"`
+	AWSSQSQueueNameNotification    string `env:"AWS_SQS_QUEUE_NAME_NOTIFICATION,required"`
+	TempPath                       string `env:"TEMP_PATH,required"`
+	DBHost                         string `env:"DB_HOST,required"`
+	DBPort                         int    `env:"DB_PORT,required"`
+	DBUser                         string `env:"DB_USER,required"`
+	DBPassword                     string `env:"DB_PASSWORD,required"`
+	DBName                         string `env:"DB_NAME,required"`
+	DBSSLMode                      string `env:"DB_SSL_MODE,required"`
+	NumberWorkers                  int    `env:"NUMBER_WORKERS,required"`
 }
 
 func NewConfig() *Config {
@@ -37,22 +38,23 @@ func NewConfig() *Config {
 	}
 
 	return &Config{
-		CognitoClientSecret: os.Getenv("COGNITO_CLIENT_SECRET"),
-		CognitoClientID:     os.Getenv("COGNITO_CLIENT_ID"),
-		CognitoUserPoolID:   os.Getenv("COGNITO_USER_POOL_ID"),
-		AWSRegion:           os.Getenv("AWS_REGION"),
-		AWSAccessKeyID:      os.Getenv("AWS_ACCESS_KEY_ID"),
-		AWSSecretAccessKey:  os.Getenv("AWS_SECRET_ACCESS_KEY"),
-		AWSS3Bucket:         os.Getenv("AWS_S3_BUCKET"),
-		AWSSQSQueueName:     os.Getenv("AWS_SQS_QUEUE_NAME"),
-		TempPath:            os.Getenv("TEMP_PATH"),
-		DBHost:              os.Getenv("DB_HOST"),
-		DBPort:              dbPort,
-		DBUser:              os.Getenv("DB_USER"),
-		DBPassword:          os.Getenv("DB_PASSWORD"),
-		DBName:              os.Getenv("DB_NAME"),
-		DBSSLMode:           os.Getenv("DB_SSL_MODE"),
-		NumberWorkers:       workers,
+		CognitoClientSecret:            os.Getenv("COGNITO_CLIENT_SECRET"),
+		CognitoClientID:                os.Getenv("COGNITO_CLIENT_ID"),
+		CognitoUserPoolID:              os.Getenv("COGNITO_USER_POOL_ID"),
+		AWSRegion:                      os.Getenv("AWS_REGION"),
+		AWSAccessKeyID:                 os.Getenv("AWS_ACCESS_KEY_ID"),
+		AWSSecretAccessKey:             os.Getenv("AWS_SECRET_ACCESS_KEY"),
+		AWSS3Bucket:                    os.Getenv("AWS_S3_BUCKET"),
+		AWSSQSQueueNameVideoProcessing: os.Getenv("AWS_SQS_QUEUE_NAME_VIDEO_PROCESSING"),
+		AWSSQSQueueNameNotification:    os.Getenv("AWS_SQS_QUEUE_NAME_NOTIFICATION"),
+		TempPath:                       os.Getenv("TEMP_PATH"),
+		DBHost:                         os.Getenv("DB_HOST"),
+		DBPort:                         dbPort,
+		DBUser:                         os.Getenv("DB_USER"),
+		DBPassword:                     os.Getenv("DB_PASSWORD"),
+		DBName:                         os.Getenv("DB_NAME"),
+		DBSSLMode:                      os.Getenv("DB_SSL_MODE"),
+		NumberWorkers:                  workers,
 	}
 }
 
@@ -121,8 +123,12 @@ func (c *Config) ValidateSQSConfig() error {
 		return fmt.Errorf("AWS secret access key is required")
 	}
 
-	if c.AWSSQSQueueName == "" {
-		return fmt.Errorf("AWS SQS queue name is required")
+	if c.AWSSQSQueueNameVideoProcessing == "" {
+		return fmt.Errorf("AWS SQS video processing queue name is required")
+	}
+
+	if c.AWSSQSQueueNameNotification == "" {
+		return fmt.Errorf("AWS SQS notification queue name is required")
 	}
 
 	return nil
