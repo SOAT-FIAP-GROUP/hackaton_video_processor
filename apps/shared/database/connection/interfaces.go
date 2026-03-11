db4a0643594d0df4646425769846afa030792ae9
Rogerio Inacio <rogerio.reaper@gmail.com>
2026-03-10 22:06:32 -0300

    feat: add method to run migrations

--- apps/shared/database/connection/interfaces.go
+++ apps/shared/database/connection/interfaces.go
@@ -3,10 +3,12 @@ package connection
 import (
 	"context"
 	"database/sql"
+	"shared/database/connection/migrations"
 )
 
 type DatabaseConnection interface {
 	Test() error
 	Close() error
 	QueryRow(ctx context.Context, query string, scan func(*sql.Rows) error, args ...interface{}) error
+	RunMigrations(migrations []migrations.Migration) error
 }
