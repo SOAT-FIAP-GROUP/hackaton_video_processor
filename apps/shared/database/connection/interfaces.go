9bfb2660d3b75c312f84265bb2aaedea8cada619
Rogerio Inacio <rogerio.reaper@gmail.com>
2026-03-12 13:09:05 -0300

    feat: add query rows method

--- apps/shared/database/connection/interfaces.go
+++ apps/shared/database/connection/interfaces.go
@@ -10,5 +10,6 @@ type DatabaseConnection interface {
 	Test() error
 	Close() error
 	QueryRow(ctx context.Context, query string, scan func(*sql.Rows) error, args ...interface{}) error
+	QueryRows(ctx context.Context, query string, scan func(*sql.Rows) error, args ...interface{}) error
 	RunMigrations(migrations []migrations.Migration) error
 }
